d8a9b091711c815f7f6c8135e75092dbaee29e20
aopogithub <fanaoao@hotmail.com>
2024-12-26 21:44:29 +0800

    modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
-from flask_wtf import CSRFProtect
-import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/baby_feeding_tracker.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.urandom(24) # 添加这一行，设置秘密密钥
 
 db = SQLAlchemy(app)
-csrf = CSRFProtect(app)
 
 from app import routes,models
 from app.routes import app as routes_blueprint
