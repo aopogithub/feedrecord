8ea87bbb764bdd8ce3b231affa77a2bcdc05f709
aopogithub <fanaoao@hotmail.com>
2024-12-26 21:50:16 +0800

    modified:   app/forms.py  No csrf

--- app/forms.py
+++ app/forms.py
@@ -7,6 +7,10 @@ class FeedingRecordForm(FlaskForm):
     amount = IntegerField('喂养量 (毫升)', validators=[DataRequired(), NumberRange(min=1)])
     notes = StringField('备注')
     submit = SubmitField('添加记录')
+    class Meta:
+        csrf = False
 
 class DeleteRecordForm(FlaskForm):
     submit = SubmitField('确认删除')
+    class Meta:
+        csrf = False
